ef50b43f42c78eaaf8f1a796eafadef9f50d4493
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-09-08 13:31:41 -0400

    Retrieved health risk scores of each city

--- Assignments/Homework1/urban_air_quality_dataset_exploration.R
+++ Assignments/Homework1/urban_air_quality_dataset_exploration.R
@@ -1,2 +1,12 @@
 getwd()
+#read dataset
 airquality_data <- read.csv('airQuality.csv')
+
+tapply(airquality_data$Temp_Range, airquality_data$City, mean)
+#average temperature range for each city
+#Chicago has an average Temp_Range of 16.50.
+#Dallas has an average Temp_Range of 17.79.
+#Phoenix has an average Temp_Range of 18.56.
+tapply(airquality_data$Health_Risk_Score, airquality_data$City, mean)
+#average health risk scores of each city 
+#higher risk score = higher health impact, caused by poor air quality 
