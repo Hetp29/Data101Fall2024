c92ee4da27a069626425248d5aff4843c7c3dbd2
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-09-21 16:18:12 -0400

    Removed unecessary files

--- Demo/GettingStarted.R
+++
@@ -1,8 +0,0 @@
-getwd()
-#wd is where R has been initialized 
-#getwd() stands for get working directory
-moody <- read.csv('moody2022.csv') #read csv file with above line of code
-head(moody) 
-
-getwd()
-
