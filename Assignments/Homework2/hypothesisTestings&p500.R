883554aee94be611228fdd866a0d533eb3d87428
Het Patel <hetp0322@gmail.com>
2024-10-12 16:35:49 -0400

    Completed 2nd and 3rd hypothesis tesst

--- Assignments/Homework2/hypothesisTestings&p500.R
+++ Assignments/Homework2/hypothesisTestings&p500.R
@@ -10,8 +10,33 @@ summary(sp500_data)
 
 colnames(sp500_data)
 
-#HYPOTHESIS 1
-#Null hypothesis: There is no significant difference in market cap between companies in technology and healthcare sectors.
-#Alternative hypothesis: There is a significant difference in market cap between companies in technology and healthcare sectors.
-p_value1 <- permutation_test(sp500_data, 'Sector', 'Marketcap', 10000, 'Technology', 'Healthcare')
-cat("The p-value for Hypothesis 3 (Strong Alternative) is:", p_value1)
+tapply(sp500_data$Marketcap, sp500_data$Sector, mean)
+tapply(sp500_data$Currentprice, sp500_data$Sector, mean)
+tapply(sp500_data$Fulltimeemployees, sp500_data$Sector, mean)
+
+#HYPOTHESIS 1 (small p-value/strong alternative hypothesis, much less than 0.05)
+#Null hypothesis: There is no significant difference in the number of full-time employees between Technology and Consumer Defensive sectors.
+#Alternative hypothesis: There is a significant difference in the number of full-time employees between Technology and Consumer Defensive sectors.
+p_value1 <- permutation_test(sp500_data, 'Sector', 'Fulltimeemployees', 50000, 'Technology', 'Consumer Defensive')
+cat("The significance level for this test is: 0.05\n")
+cat("The p-value for hypothesis 1 (very low p-value) is:", p_value1, "\n")
+#p-value is 0.00576, we can reject null hypothesis with confidence
+#There is a significant difference in the number of full-time employees between companies in Technology and Consumer Defensive sectors.
+
+#HYPOTHESIS 2 (close call, p-value less than 0.05 but close)
+#Null hypothesis: There is no difference in the current price between companies in the energy and industrials sectors.
+#Alternative hypothesis: There is a significant difference in the current price between companies in the energy and industrials sectors.
+p_value2 <- permutation_test(sp500_data, 'Sector', 'Currentprice', 50000, 'Energy', 'Industrials')
+cat("The significance level for this test is: 0.05\n")
+cat("The p-value for hypothesis 2 (close call) is:", p_value2, "\n")
+#p-value is 0.03896, we can reject null hypothesis but this one is a close call!
+#there is enough evidence to declare that there is a significant difference in current price between companies in energy and industrials sectors.
+
+#HYPOTHESIS 3 (reject null hypothesis, p-value exceeds 0.05)
+#Null hypothesis: There is no significant difference in market cap between companies in the consumer defensive and healthcare sectors. 
+#Alternative hypothesis: There is a significant difference in market cap between companies in the consumer defensive and healthcare sectors.
+p_value3 <- permutation_test(sp500_data, 'Sector', 'Marketcap', 50000, 'Consumer Defensive', 'Healthcare')
+cat("The significance level for this test is: 0.05\n")
+cat("The p-value for hypothesis 3 (rejecting the null hypothesis) is:", p_value3, "\n")
+#p-value is 0.42626, we cannot reject the null hypothesis 
+#there is no significant difference in market cap between companies in consumer defensive and healthcare sectors
