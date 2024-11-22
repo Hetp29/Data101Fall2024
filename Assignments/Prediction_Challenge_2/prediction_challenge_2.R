955c8e8c2fe8a692d81f3b67113a40c3cfc7d392
Het Patel <hetp0322@gmail.com>
2024-11-22 17:57:05 -0500

    84% accuracy

--- Assignments/Prediction_Challenge_2/prediction_challenge_2.R
+++ Assignments/Prediction_Challenge_2/prediction_challenge_2.R
@@ -12,56 +12,24 @@ boxplot(GroomInc ~ Outcome, data = train, main = "Groom's Income by Outcome")
 boxplot(BrideInc ~ Outcome, data = train, main = "Bride's Income by Outcome")
 table(train$GroomMB, train$Outcome)
 
-tree <- rpart(Outcome ~ GroomMB + BrideMB + GroomInc + BrideInc, data = train, method = "class")
-summary(tree)
-prediction <- predict(tree, train, type="class")
-accuracy <- mean(prediction == train$Outcome)
-print(paste("Training Accuracy:", accuracy))
-misclassified <- train[prediction != train$Outcome, ]
-table(misclassified$GroomMB, misclassified$BrideMB)
 
 
 
 
-# Add Income Difference column to the training data
-train$IncomeDiff <- abs(train$GroomInc - train$BrideInc)
 
-# Boxplot to visualize how IncomeDiff varies by Outcome
+train$IncomeDiff <- train$GroomInc - train$BrideInc
 boxplot(IncomeDiff ~ Outcome, data = train, main = "Income Difference by Outcome")
 
-# Summary statistics for IncomeDiff
-aggregate(IncomeDiff ~ Outcome, data = train, summary)
 
-
-
-# Add Total Income column to the training data
 train$TotalInc <- train$GroomInc + train$BrideInc
-
-# Boxplot to visualize how TotalInc varies by Outcome
 boxplot(TotalInc ~ Outcome, data = train, main = "Total Income by Outcome")
 
-# Summary statistics for TotalInc
-aggregate(TotalInc ~ Outcome, data = train, summary)
 
-
-
-# Create a new column for the MBTI pair
-train$MBPair <- paste(train$GroomMB, train$BrideMB, sep = "_")
-
-# Frequency table of MBTI pairs and outcomes
-table(train$MBPair, train$Outcome)
-
-# Subset analysis for common misclassified MBTI pairs
-misclassified_pairs <- train[prediction != train$Outcome, ]
-table(misclassified_pairs$MBPair)
-
-
-# Train the decision tree with new features
-tree <- rpart(Outcome ~ GroomMB + BrideMB + GroomInc + BrideInc + IncomeDiff + TotalInc + MBPair,
+tree <- rpart(Outcome ~ GroomMB + BrideMB + GroomInc + BrideInc + IncomeDiff + TotalInc,
               data = train, method = "class",
               control = rpart.control(minsplit = 50, minbucket = 25, cp = 0.01))
 
-# Check the training accuracy
 prediction <- predict(tree, train, type = "class")
 accuracy <- mean(prediction == train$Outcome)
 print(paste("Updated Training Accuracy:", accuracy))
+
