51e5e4c2380397cd859d9ee782fc2a43732f75dc
Het Patel <hetp0322@gmail.com>
2024-11-22 21:42:32 -0500

    94.28% accuracy on training data

--- Assignments/Prediction_Challenge_2/prediction_challenge_2.R
+++ Assignments/Prediction_Challenge_2/prediction_challenge_2.R
@@ -1,8 +1,10 @@
 library(rpart)
+devtools::install_github("devanshagr/CrossValidation")
 getwd()
 setwd("/Users/hetpatel/Data101Fall2024/Data101Fall2024/Assignments/Prediction_Challenge_2")
 train <- read.csv("couplesTrain.csv")
 test <- read.csv("CouplesTestStudents.csv")
+submission <- read.csv('https://raw.githubusercontent.com/paramshah4/data101_tutorial/main/files/dataset/submissionC.csv')
 str(train)
 summary(train)
 colnames(train)
@@ -25,65 +27,18 @@ train$TotalInc <- train$GroomInc + train$BrideInc
 boxplot(TotalInc ~ Outcome, data = train, main = "Total Income by Outcome")
 
 
+
 tree <- rpart(Outcome ~ GroomMB + BrideMB + GroomInc + BrideInc + IncomeDiff + TotalInc,
               data = train, 
               method = "class",
-              control = rpart.control(minsplit = 5, minbucket = 5, cp = 0.005))
+              control = rpart.control(minsplit = 5, minbucket = 5, cp = 0.0002))
 
 prediction <- predict(tree, train, type = "class")
 accuracy <- mean(prediction == train$Outcome)
 print(paste("Updated Training Accuracy:", accuracy))
 
+test_predictions <- predict(tree, test, type = "class")
+test$Outcome <- test_predictions
 
-
-
-
-
-
-cross_validate <- function(data, formula, n_iter = 10, split_ratio = 0.8, minsplit = 5, minbucket = 5, cp = 0.005) {
-  accuracies <- numeric(n_iter)
-  
-  for (i in 1:n_iter) {
-    # Split data into training and validation subsets
-    train_idx <- sample(seq_len(nrow(data)), size = floor(split_ratio * nrow(data)))
-    train_subset <- data[train_idx, ]
-    val_subset <- data[-train_idx, ]
-    
-    # Train the decision tree
-    tree <- rpart(formula, 
-                  data = train_subset, 
-                  method = "class",
-                  control = rpart.control(minsplit = minsplit, minbucket = minbucket, cp = cp))
-    
-    # Predict on validation subset
-    val_prediction <- predict(tree, val_subset, type = "class")
-    val_accuracy <- mean(val_prediction == val_subset$Outcome)
-    
-    # Store accuracy
-    accuracies[i] <- val_accuracy
-  }
-  
-  # Return the mean accuracy and accuracies from each iteration
-  list(mean_accuracy = mean(accuracies), accuracies = accuracies)
-}
-
-# Perform cross-validation
-formula <- Outcome ~ GroomMB + BrideMB + GroomInc + BrideInc + IncomeDiff + TotalInc
-cv_results <- cross_validate(data = train, formula = formula, n_iter = 10, split_ratio = 0.8, minsplit = 5, minbucket = 5, cp = 0.005)
-
-# Print results
-print(paste("Cross-Validation Mean Accuracy:", cv_results$mean_accuracy))
-print("Accuracies from each iteration:")
-print(cv_results$accuracies)
-
-# Train the final tree on the full training data
-final_tree <- rpart(formula, 
-                    data = train, 
-                    method = "class",
-                    control = rpart.control(minsplit = 5, minbucket = 5, cp = 0.005))
-
-# Predict on the full training data to check training accuracy
-final_prediction <- predict(final_tree, train, type = "class")
-final_accuracy <- mean(final_prediction == train$Outcome)
-print(paste("Final Training Accuracy:", final_accuracy))
-
+submission$Outcome <- test$Outcome
+write.csv(submission, "submission.csv", row.names = FALSE)
